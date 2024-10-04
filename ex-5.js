5d80e1659c7b6ab88f4c5720125f9b1a34ff2378
Bunyawat Singkiporn <pupahero@gmail.com>
2024-10-04 13:12:30 +0700

    ex1-6

--- ex-5.js
+++ ex-5.js
@@ -1,4 +1,7 @@
 // Exercise #5: Concatenate the Alphabets
 let companyName = "TechUp";
-
+for (let i = 0; i < companyName.length; i++) {
+    let order = i + 1; 
+    console.log(`Number ${order} character is ${companyName[i]}`);
+}
 // Start coding here
